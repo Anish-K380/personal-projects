6fd540de0780362a5033ab6373ef2913cb2e331e
Anish <anishkumar.s2005@gmail.com>
2025-12-01 03:23:47 +0530

    Updated date part in stockSimple

--- stockSimple.py
+++ stockSimple.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import date
+from datetime import date, timedelta
 from collections import deque
 
 class Stock:
@@ -7,30 +7,64 @@ class Stock:
         self.name = input('Enter ticker of stock:')
         self.company = input('Enter company of stock:')
         self.startDate = date.today()
-        self.prices = deque()
+        self.endDate = date.today()
+        self.prices = deque((0,))
 
-    def changeStartDate(self):
-        newDate = dateInput('Enter date in format (dd.mm.yyyy):')
+    def changeStartDate(self, newDate = None):
+        if newDate is None:
+            newDate = dateInput('Enter date in format (dd.mm.yyyy):')
+        if newDate > self.endDate:
+            self.startDate = newDate
+            self.endDate = newDate
+            self.prices = deque((0,))
         difference = (newDate - self.startDate).days
-        default = 0
-        if len(self.prices) != 0:
-            default = self.prices[0]
-        while difference < 0:
-            difference += 1
-            self.prices.appendleft(default)
+        default = self.prices[0]
         if difference >= len(self.prices):
+            self.endDate = newDate
+            self.prices = deque((0,))
             difference = 0
-            self.prices = deque()
+        while difference > 0:
+            self.prices.popleft()
+            difference -= 1
+        while difference < 0:
+            self.prices.appendleft(default)
+            difference += 1
         self.startDate = newDate
+
+    def changeEndDate(self, newDate = None):
+        if newDate is None:
+            newDate = dateInput('Enter date in format (dd.mm.yyyy):')
+        if newDate < self.startDate:
+            self.startDate = newDate
+            self.endDate = newDate
+            self.prices = deque((0,))
+        difference = (self.endDate - newDate).days
+        default = self.prices[-1]
+        if difference >= len(prices):
+            self.startDate = newDate
+            self.prices = deque((0,))
+            difference = 0
+        while difference > 0:
+            self.prices.pop()
+            difference -= 1
+        while difference < 0:
+            self.prices.append(default)
+            difference += 1
+        self.endDate = newDate
 
     def stockPriceEdit(self):
         dateStart = dateInput('Enter start range of date in format (dd.mm.yyyy):')
         dateEnd = dateInput('Enter end range of date in format (dd.mm.yyyy):')
-        leftAdd = (self.startDate - dateStart).days
-        while leftAdd > 0:
+        if dateStart > dateEnd:
+            print('Just to inform you, you switched the dates around. I can handle it though.')
+            dateStart, dateEnd = dateEnd, dateStart
+        if dateStart < self.startDate:
+            self.changeStartDate(dateStart)
+        if dateEnd > self.endDate:
+            self.changeEndDate(dateEnd)
         print('Enter prices corresponding with date')
-        for i in range((dateEnd - dateStart).days + 1):
-            print
+        for i in range((dateStart - self.startDate).days, (dateEnd - self.startDate).days + 1):
+            self.prices[i] = float(input((self.startDate + timedelta(days = i)).strftime('%d.%m.%Y :')))
     
 def dateInput(message):
     dateTuple = tuple(map(int, input(message).split('.')))
